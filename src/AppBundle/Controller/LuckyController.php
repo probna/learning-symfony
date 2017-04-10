783ad2cca17fe03850ea032f8915a942ccac1c4b
Tomo <hcs.omot@gmail.com>
2017-04-10 22:56:17 +0200

    Practice with forms, controllers, routing, oh my

--- src/AppBundle/Controller/LuckyController.php
+++ src/AppBundle/Controller/LuckyController.php
@@ -9,13 +9,16 @@ use Symfony\Component\HttpFoundation\Response;
 class LuckyController extends Controller
 {
     /**
+     * @Route("/lucky/number/")
      * @Route("/lucky/number/{max}", requirements={"max": "\d*"})
      * @param $max
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function numberAction($max = 50)
+    public function numberAction( $max = 50)
     {
+        var_dump($max);
+        die();
         $number = mt_rand(0, $max);
 
         return $this->render('lucky/number.html.twig', array(
