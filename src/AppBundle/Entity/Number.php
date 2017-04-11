4937550d34865d2c57a7bf8476a6ab3322326349
Tomo Šala <tomo.sala@gmail.com>
2017-04-11 11:31:24 +0200

    Removed .idea

---
+++ src/AppBundle/Entity/Number.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Number
+ *
+ * @ORM\Table(name="number")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\NumberRepository")
+ */
+class Number
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="value", type="integer")
+     */
+    private $value;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set value
+     *
+     * @param integer $value
+     *
+     * @return Number
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return int
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
+
