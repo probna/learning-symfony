ff837ff0bb35e551f217a9e89192366bc7910e05
Tomo Šala <hcs.omot@gmail.com>
2017-05-13 21:51:50 +0200

    Use default controller to show all airports
    Created a simple corresponding view, just to display a list of airports

--- src/Aviation/AirlinesBundle/Controller/DefaultController.php
+++ src/Aviation/AirlinesBundle/Controller/DefaultController.php
@@ -7,17 +7,19 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller {
     /**
-     * Shows a form for selecting departure and destination airports
+     * Show all airports
      *
      * @Route("/")
      */
     public function indexAction()
     {
 
-       $airlines = $this->get('aviation.airlines.service.sexy_airlines.top5')->getAirlinesByCountry(1);
+        $em = $this->getDoctrine()->getManager();
+
+        $airports = $em->getRepository('AviationAirlinesBundle:Airport')->findAll();
 
         return $this->render('AviationAirlinesBundle:Default:index.html.twig', array(
-            'airlines' => $airlines,
+            'airports' => $airports,
         ));
     }
 }
