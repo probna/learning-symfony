a48f11761f03a72c751403d222ff0a0b6efcc6b9
Tomo Šala <hcs.omot@gmail.com>
2017-05-18 17:40:10 +0200

    Create basic search form class and template
    Renamed the FindFlights class to FindFlightsType, fixed errors,
    created a simple template for displaying the search form.

---
+++ src/Aviation/AirlinesBundle/Form/FindFlightsType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Aviation\AirlinesBundle\Form;
+
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+
+class FindFlightsType extends AbstractType {
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('From', TextType::class)
+            ->add('To', TextType::class)
+            ->add('On', DateType::class)
+            ->add('Search', SubmitType::class);
+    }
+
+
+}
