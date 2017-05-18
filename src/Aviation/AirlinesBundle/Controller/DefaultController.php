55038d863e1b83860fb0a811c4c3228c6df5bab4
Tomo Šala <hcs.omot@gmail.com>
2017-05-18 17:42:57 +0200

    Process search form
    Rewrote index action to display search form, added a separate method
    for processing the form submit, and extracted the original functionality
    (displaying the basic list of all airports) into a backup method.

--- src/Aviation/AirlinesBundle/Controller/DefaultController.php
+++ src/Aviation/AirlinesBundle/Controller/DefaultController.php
@@ -2,8 +2,13 @@
 
 namespace Aviation\AirlinesBundle\Controller;
 
+use Aviation\AirlinesBundle\Form\FindFlightsType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\VarDumper\VarDumper;
 
 class DefaultController extends Controller {
     /**
@@ -11,9 +16,49 @@ class DefaultController extends Controller {
      *
      * @Route("/")
      */
-    public function indexAction()
+    public function indexAction(): Response
     {
 
+        $form = $this->createForm(FindFlightsType::class, null, ['action' => $this->generateUrl('search-submit')]);
+        
+        return $this->render('AviationAirlinesBundle:Default:findFlightsForm.html.twig',
+            ['flights_form' => $form->createView()]);
+
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @Method("POST")
+     * @Route("/search-submission", name="search-submit")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handleSearchRequest(Request $request)
+    {
+        $form = $this->createForm(FindFlightsType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+
+            $airports = $em->getRepository('AviationAirlinesBundle:Airport')->findAll();
+
+            return $this->render('AviationAirlinesBundle:Default:index.html.twig', array(
+                'airports' => $airports,
+            ));
+        }
+    }
+
+
+    /**
+     * Shows a list of all airports
+     *
+     * @Route("/get-airports")
+     */
+    public function getListAction()
+    {
         $em = $this->getDoctrine()->getManager();
 
         $airports = $em->getRepository('AviationAirlinesBundle:Airport')->findAll();
