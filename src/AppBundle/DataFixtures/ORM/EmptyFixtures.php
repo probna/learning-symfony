4af5b5bd4ea253fc62b625215b9d6567a8d36346
Miro Svrtan <miro@mirosvrtan.me>
2017-06-07 21:24:30 +0200

    Add fixture

---
+++ src/AppBundle/DataFixtures/ORM/EmptyFixtures.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Aviation\AirlinesBundle\Entity\Airline;
+use Aviation\AirlinesBundle\Entity\Airport;
+use Aviation\AirlinesBundle\Entity\Country;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class EmptyFixtures extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $croatia = new Country();
+        $croatia->setCountryName('Croatia');
+        $croatia->setCountryCode('HR');
+
+        $manager->persist($croatia);
+
+        $lufthansa = new Airline();
+        $lufthansa->setName('Luthansa');
+        $lufthansa->setCountry($croatia);
+
+        $manager->persist($lufthansa);
+
+        $pleso = new Airport();
+        $pleso->setCountry($croatia);
+        $pleso->setName('Pleso');
+        $pleso->setLocation('GPS');
+
+        $manager->persist($pleso);
+
+        //$obj = new \stdClass();
+        //$manager->persist($obj);
+
+        $manager->flush();
+        //$this->addReference('obj', $obj);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOrder()
+    {
+        return 10;
+    }
+}
