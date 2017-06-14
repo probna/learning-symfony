921e2c3411e2dc1d3cfeab687d346d3592fb14e8
Tomo Šala <hcs.omot@gmail.com>
2017-06-14 15:44:42 +0200

    Move/reimplement Country controller
    Moved the complete Country controller functionality to new location.
    Since all the country functionality is now isolated in a separate
    bundle, we can use the new bundle's Default controller to implement it.

--- src/Aviation/CountryBundle/Controller/DefaultController.php
+++ src/Aviation/CountryBundle/Controller/DefaultController.php
@@ -2,16 +2,45 @@
 
 namespace Aviation\CountryBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Aviation\CountryBundle\Entity\Country;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * Country controller.
+ *
+ * @Route("country")
+ */
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * Lists all country entities.
+     *
+     * @Route("/", name="country_index")
+     * @Method("GET")
      */
     public function indexAction()
     {
-        return $this->render('AviationCountryBundle:Default:index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+
+        $countries = $em->getRepository('AviationCountryBundle:Country')->findAll();
+
+        return $this->render('AviationCountryBundle:Default:index.html.twig', [
+            'countries' => $countries,
+        ]);
+    }
+
+    /**
+     * Finds and displays a country entity.
+     *
+     * @Route("/{id}", name="country_show")
+     * @Method("GET")
+     */
+    public function showAction(Country $country)
+    {
+        return $this->render('AviationCountryBundle:Default:show.html.twig', [
+            'country' => $country,
+        ]);
     }
 }
