5fd5f70977bfc76f80231fdb1309f16d56f5025c
Tomo <hcs.omot@gmail.com>
2017-06-15 12:12:52 +0200

    Add migrations file for flight fields
    Flight should have not nullable fields.

---
+++ app/DoctrineMigrations/Version20170615101119.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170615101119 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE flight CHANGE airline airline INT NOT NULL, CHANGE departureAirport departureAirport INT NOT NULL, CHANGE arrivalAirport arrivalAirport INT NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE flight CHANGE airline airline INT DEFAULT NULL, CHANGE departureAirport departureAirport INT DEFAULT NULL, CHANGE arrivalAirport arrivalAirport INT DEFAULT NULL');
+    }
+}
